6439013e49c66a3612739f4b6680603f16172224
karimgaberhd12 <karimgabernew@gmail.com>
2025-09-06 14:17:51 +0300

    Crypto-dash

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import HomePage from "./pages/HomePage";
+import { Routes, Route } from "react-router";
+import AboutPage from "./pages/About";
+import CoinDetailsPage from "./pages/Coin-details";
+import NotFoundPage from "./pages/Not-Found";
+const API_URL = import.meta.env.VITE_API_URL;
+function App() {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [limit, setLimit] = useState(10);
+  const [filter, setFilter] = useState("");
+  const [sortBy, setSortBy] = useState("market_cap_desc");
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const res = await fetch(
+          `${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`
+        );
+        if (!res.ok) throw new Error("failed to fetch data");
+        const data = await res.json();
+        console.log(data);
+        setCoins(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCoins();
+  }, [limit]);
+
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <HomePage
+            coins={coins}
+            filter={filter}
+            setFilter={setFilter}
+            limit={limit}
+            setLimit={setLimit}
+            sortBy={sortBy}
+            setSortBy={setSortBy}
+            loading={loading}
+            error={error}
+          />
+        }
+      />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/coin/:id" element={<CoinDetailsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+}
+
+export default App;
